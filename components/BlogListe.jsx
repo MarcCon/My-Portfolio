101dc253e0e4442ad6b51db6f9b5c37a5439fc55
Marc-Constantin Enke <marc.enke@aol.de>
2024-04-14 17:00:31 +0200

    Logik für einen Blogartikel bei xs

--- components/BlogListe.jsx
+++ components/BlogListe.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { GraphQLClient, gql } from "graphql-request";
-import BlogCard from "./BlogCard"; // Stelle sicher, dass der Pfad korrekt ist
+import BlogCard from "./BlogCard";
+import { useMediaQuery } from "react-responsive";
 
 const graphcms = new GraphQLClient(process.env.GRAPHQL_ENDPOINT);
 
@@ -21,6 +22,8 @@ const QUERY = gql`
 const BlogListe = () => {
   const [posts, setPosts] = useState([]);
 
+  const isXs = useMediaQuery({ maxWidth: 634 });
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -36,15 +39,35 @@ const BlogListe = () => {
 
   return (
     <div className="flex justify-center gap-16 flex-wrap mb-72">
-      {posts.map((post) => (
-        <BlogCard
-          key={post.id}
-          title={post.title}
-          coverPhoto={post.coverPhoto}
-          datePublished={post.datePublished}
-          slug={post.slug}
-        />
-      ))}
+      {posts.map((post, index) => {
+        // Überprüfe, ob es xs ist und ob es das letzte Element ist
+        if (isXs) {
+          // Bei xs, nur das letzte Element rendern
+          if (index === posts.length - 1) {
+            return (
+              <BlogCard
+                key={post.id}
+                title={post.title}
+                coverPhoto={post.coverPhoto}
+                datePublished={post.datePublished}
+                slug={post.slug}
+              />
+            );
+          }
+          return null;
+        } else {
+          // Bei Nicht-xs, alle Elemente rendern
+          return (
+            <BlogCard
+              key={post.id}
+              title={post.title}
+              coverPhoto={post.coverPhoto}
+              datePublished={post.datePublished}
+              slug={post.slug}
+            />
+          );
+        }
+      })}
     </div>
   );
 };
